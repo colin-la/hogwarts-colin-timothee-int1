0c889de8749f723fbe0447e67089e3ac81308906
Timothee <syrokvachine07@gmail.com>
2025-12-03 15:50:54 +0100

    Added meet_hagrid function (NOT tested because Colin's function is broken)

--- chapter/chapter1.py
+++ chapter/chapter1.py
@@ -1,3 +1,5 @@
+from utils.input_utils import *
+from universe.character import *
 
 def introduction():
     pass
@@ -9,7 +11,12 @@ def receive_letter():
     pass
 
 def meet_hagrid(character):
-    pass
+    choice = ask_choice("Hagrid: 'Hello {}! I’m here to help you with your shopping on Diagon Alley.\n\n\
+          Do you want to follow Hagrid?".format(character['First Name']), [1, 2])
+    if choice == 2:
+        print("Hagrid gently insists and takes you along anyway!")
+    else:
+        print("You follow Hagrid to Diagon Alley!")
 
 def buy_supplies(character):
     pass
@@ -17,3 +24,10 @@ def buy_supplies(character):
 def start_chapter_1():
     pass
 
+#################
+##### Tests #####
+#################
+'''
+Harry = init_character("Potter", "Harry", {"Courage": 8, "Intelligence": 8, "Loyalty": 8, "Ambition": 8})
+meet_hagrid(Harry)
+'''
