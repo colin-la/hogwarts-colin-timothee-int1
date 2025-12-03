d285ae41dcda1166ec1ca0f395efb830590022ed
Colin <colin.larese@protonmail.com>
2025-12-03 15:46:01 +0100

    Save but chapter1 doesn't recognize other files

--- chapter/chapter1.py
+++ chapter/chapter1.py
@@ -1,9 +1,28 @@
-
+from universe.character import *
+from utils.input_utils import *
 def introduction():
-    pass
+    input("Welcome to you, stranger. You will live an amazing story \n \
+        Without further a do, let's get started ! \n \
+        Press Enter to continue ...")
 
 def create_character():
-    pass
+    last_name = ask_text("Enter your character's last name: ")
+    first_name = ask_text("Enter your character's first name: ")
+    print("Choose your attributes: ")
+    courage = ask_number("Courage level (1-10): ")
+    intelligence = ask_number("Intelligence level (1-10): ")
+    loyalty = ask_number("Loyalty level (1-10): ")
+    ambition = ask_number("Ambition level (1-10): ")
+    print(f"Character profile: \n \
+        Last name: {last_name} \n \
+        First name: {first_name} \n \
+        Money: 100 \n \
+        Inventory: \n \
+        Spells: \n \
+        Attributes: \n \
+        \t - Courage: {courage} \n \
+        \t - Intelligence {intelligence}")
+
 
 def receive_letter():
     pass
@@ -17,3 +36,6 @@ def buy_supplies(character):
 def start_chapter_1():
     pass
 
+if __name__ == "__main__":
+    introduction()
+    create_character()
