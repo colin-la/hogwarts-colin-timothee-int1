3a45fbc7fbc7a2e986eb3650f08e02e60fdcccf6
Colin <colin.larese@protonmail.com>
2025-12-03 14:03:57 +0100

    sauvegarde

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,6 +1,21 @@
 def ask_text(message: str) -> str:
     anwser = ""
     while anwser == "":
-        anwser = str(input(message)).strip()
+        anwser = str(input(message+" ")).strip()
     return anwser
 
+question = ask_text("Hello")
+
+def ask_number(message: str, min_val=None, max_val=None) -> int:
+    anwser = input(message)
+    while True:
+
+
+
+
+def ask_choice(message: str, options):
+    pass
+
+
+def load_file (file_path: str):
+    pass
