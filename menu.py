eaca30d0d723301c216112b3eaa7853a76c188b7
Colin <colin.larese@protonmail.com>
2025-12-10 14:45:56 +0100

    launch_menu_choice and display_main_menu in menu.py finished

---
+++ menu.py
@@ -0,0 +1,21 @@
+import sys
+from utils.input_utils import *
+from chapter.chapter1 import *
+def display_main_menu() -> int:
+    return ask_choice("", ["Start Chapter 1 – Arrival in the magical world.", "Exit the game."])
+
+def launch_menu_choice():
+    houses = {}
+    while True :
+        choice = display_main_menu()
+        if choice == 1:
+            dico = start_chapter1()
+            # houses = start_chapter2()
+            # start_chapter3()
+            # start_chapter4()
+        elif choice == 2:
+            print("Too bad... \nGoodbye and see you next time !")
+            break
+
+if __name__ == "__main__":
+    launch_menu_choice()
