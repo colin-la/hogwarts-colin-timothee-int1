a3cfe415ebf864f41d87666cda8ea991c6117ccf
Timothee <syrokvachine07@gmail.com>
2025-12-10 15:21:08 +0100

    created update_house_points (untested)

--- universe/house.py
+++ universe/house.py
@@ -1,5 +1,19 @@
-
-
+def update_house_points(houses: dict, house_name: str, points: int) -> None:
+    """
+    Updates a specific house's points.
+    update_house_points({"Gryffindor": 0}, 'Gryffindor', -6) will change the dictionary to {"Gryffindor": -6}
+    """
+    if house_name not in houses:
+        # warning message
+        print("WARNING: The house's name you're trying to update is NOT in the dictionary provided.")
+        return
+    houses[house_name] += points
+    if points < 0:
+        context = "lost"
+    else:
+        context = "gained"
+    print("The house {} has {} {} point(s)!".format(house_name, context, points))
+    print("It now has {} point(s).".format(houses[house_name]))
 
 
 
