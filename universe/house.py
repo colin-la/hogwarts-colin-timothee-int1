f78e6aeeeb989f2ae432b1413331bd854703b0d7
Timothee <syrokvachine07@gmail.com>
2025-12-10 15:27:41 +0100

    created display_winning_house (untested)

--- universe/house.py
+++ universe/house.py
@@ -1,3 +1,5 @@
+from math import inf
+
 def update_house_points(houses: dict, house_name: str, points: int) -> None:
     """
     Updates a specific house's points.
@@ -14,6 +16,16 @@ def update_house_points(houses: dict, house_name: str, points: int) -> None:
         context = "gained"
     print("The house {} has {} {} point(s)!".format(house_name, context, points))
     print("It now has {} point(s).".format(houses[house_name]))
+
+def display_winning_house(houses: dict) -> None:
+    maxi = -1 * inf
+    for points in houses.values():
+        if maxi < points:
+            maxi = points
+    print("Here are the winning house(s):\n")
+    for house, points in houses.items():
+        if points == maxi:
+            print("- {} with {} point(s)!".format(house, points))
 
 
 
