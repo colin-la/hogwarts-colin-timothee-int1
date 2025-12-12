721d65cb725da830cb9494f61d27dfa722b52c98
Colin <colin.larese@protonmail.com>
2025-12-12 08:45:52 +0100

    Enlevé boucle while

--- menu.py
+++ menu.py
@@ -6,16 +6,15 @@ def display_main_menu() -> int:
 
 def launch_menu_choice() -> None:
     houses = {}
-    while True :
-        choice = display_main_menu()
-        if choice == 1:
-            dico = start_chapter1()
-            # houses = start_chapter2()
-            # start_chapter3()
-            # start_chapter4()
-        elif choice == 2:
-            print("Too bad... \nGoodbye and see you next time !")
-            break
+    choice = display_main_menu()
+    if choice == 1:
+        dico = start_chapter1()
+        # houses = start_chapter2()
+        # start_chapter3()
+        # start_chapter4()
+    elif choice == 2:
+        print("Too bad... \nGoodbye and see you next time !")
+        sys.exit()
 
 if __name__ == "__main__":
     launch_menu_choice()
