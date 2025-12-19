1a2b7f001cd2d82a342ec0e8c0e8be938b470025
Colin <colin.larese@protonmail.com>
2025-12-19 09:54:12 +0100

    finised attempt goal (not tested)

--- chapter/chapter4.py
+++ chapter/chapter4.py
@@ -1,3 +1,4 @@
+from random import randint, choice
 
 
 def create_team(house, team_data, is_player=False, player=None) -> dict:
@@ -12,6 +13,26 @@ def create_team(house, team_data, is_player=False, player=None) -> dict:
         for name_player in team_data:
             if name_player not in new_list_players:
                 new_list_players.append(name_player)
+
+    return result
+
+
+def attempt_goal(attacking_team, defending_team, player_is_seeker=False) -> None:
+    print(f"The attacking team is attempting to score a goal !")
+    print(f" - Attaaaaack ! says the team leader !")
+    chance_goal = randint(1, 10)
+    if chance_goal >= 6:
+        if player_is_seeker:
+            scoring_player = attacking_team["players"][0]
+        else :
+            scoring_player = choice(attacking_team["players"][1::])
+        attacking_team["score"] += 10
+        attacking_team["goals_scored"] += 1
+        print(f"This attack was amazing ! {scoring_player} scores a goal for {attacking_team["name"]} ! (+10 points)")
+    else :
+        defending_team["goals_blocked"] += 1
+        print(f"The attack wasn't strong enough ! {defending_team["name"]} blocked the attack!")
+
 
 
 
