3188552f8d18dd933f12279a59b8b3d4d4071534
Colin <colin.larese@protonmail.com>
2025-12-19 09:12:44 +0100

    init chapter 4

--- chapter/chapter4.py
+++ chapter/chapter4.py
@@ -1,6 +1,7 @@
 
 
-
+def create_team(house, team_data, is_player=False, player=None) -> dict:
+    result = {"name": house, "score": 0, "goals_scored": 0, "goals_blocked": 0, "caught_snitch": False, "players": []}
 
 
 if __name__ == "__main__":
